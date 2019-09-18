391d5b6aaf12c6a2cb1e887a9b63a8cef3a0cd0a
renatojobal <renatojobal@gmail.com>
2019-09-18 10:08:16 -0500

    Renderizando con ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const element = document.createElement('h1');
-element.innerText = 'Hellow, PLatzi Badges!';
+// const element = document.createElement('h1');
+// element.innerText = 'Hellow, PLatzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello, Platzi Badges from React!</h1>;
 
 const container = document.getElementById('app');
 
-container.appendChild(element);
+
+// ReactDOM.render(__que__, __donde__)
+ReactDOM.render(element, container);
