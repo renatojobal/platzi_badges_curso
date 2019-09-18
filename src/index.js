944b4efd2f9eafe3bb687d6a9f4ea5672803f135
renatojobal <renatojobal@gmail.com>
2019-09-18 11:44:00 -0500

    Preparado espacio de trabajo hasta la clase 10

--- src/index.js
+++ src/index.js
@@ -1,43 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const element = <h1>Hello, Platzi Badges from React!</h1>; // JSX
 
-// const element = React.createElement(elemento, Atributo o props, Hijos);
-//const element = React.createElement('a', { href: 'https://platzi.com'}, 'Ir a Platzi') // Con React, createElement
-
-const name = "Richard";
-
-//const element = React.createElement('h1', {}, `Hola soy ${name}`);
-
-// AHora con JSX
-
-const sum = () => 3 + 3;
-
-// const jsx = <h1>Hola soy, {__expresiones__}</h1>
-// const jsx = <h1>Hola soy, {0}</h1>
-
-const jsx = (
+const element = (
   <div>
     <h1>Hola soy Richard</h1>
     <p>Soy ingeniero Frontend</p>
   </div>
 );
 
-const element = React.createElement(
-    'div',
-    {},
-    React.createElement(
-        'h1',
-        {},
-        'Hola soy Richard'
-    ),
-    React.createElement(
-        'p',
-        {},
-        'Soy ingeniero De la web'
-    )
-);
 
 const container = document.getElementById("app");
 
