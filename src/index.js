8aeddcfad28d735f297d059485972364351d8348
renatojobal <renatojobal@gmail.com>
2019-09-18 10:24:12 -0500

    Aprendiendo que es JSX y porque es mejor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hellow, PLatzi Badges!';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// const container = document.getElementById('app');
+// const element = <h1>Hello, Platzi Badges from React!</h1>; // JSX
 
-// container.appendChild(element);
+// const element = React.createElement(elemento, Atributo o props, Hijos);
+//const element = React.createElement('a', { href: 'https://platzi.com'}, 'Ir a Platzi') // Con React, createElement
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+const name = "Richard";
 
-const element = <h1>Hello, Platzi Badges from React!</h1>;
+//const element = React.createElement('h1', {}, `Hola soy ${name}`);
 
-const container = document.getElementById('app');
+// AHora con JSX
 
+const sum = () => 3 + 3;
+
+// const jsx = <h1>Hola soy, {__expresiones__}</h1>
+// const jsx = <h1>Hola soy, {0}</h1>
+
+const jsx = (
+  <div>
+    <h1>Hola soy Richard</h1>
+    <p>Soy ingeniero Frontend</p>
+  </div>
+);
+
+const element = React.createElement(
+    'div',
+    {},
+    React.createElement(
+        'h1',
+        {},
+        'Hola soy Richard'
+    ),
+    React.createElement(
+        'p',
+        {},
+        'Soy ingeniero De la web'
+    )
+);
+
+const container = document.getElementById("app");
 
 // ReactDOM.render(__que__, __donde__)
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
