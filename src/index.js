9eae6e3d0e5a2c7889ad2f5add307e91753bb547
renatojobal <renatojobal@gmail.com>
2019-09-18 21:55:12 -0500

    Hasta la clase 11, creado badge usando las props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-import 'bootstrap/dist/css/bootstrap.css';
-import './global.css';
-import Badge from './components/Badge'
-
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
+import Badge from "./components/Badge";
 
 const container = document.getElementById("app");
 
 // ReactDOM.render(__que__ (elemento) No recibe componentes, __donde__)
-ReactDOM.render(<Badge/>, container);
+ReactDOM.render(
+  <Badge
+    firstName="Lilly"
+    lastName="Kaufman"
+    jobTitle="Frontend Enginner"
+    twitter="sparragus"
+    avatarUrl="https://gravatar.com/avatar?d=identicon"
+  />,
+  container
+);
