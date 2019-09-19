f3417699814e250a732472ac5af1779dd5816df4
renatojobal <renatojobal@gmail.com>
2019-09-18 21:44:48 -0500

    Hasta la clase 11, creado el Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,28 +1,33 @@
 import React from "react";
 
+import "./styles/Badge.css"; // PRimera forma de traer estilos
 import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://gravatar.com/" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+          />
           <h1>
             Richard <br /> Kaufman
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@sparragus</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@sparragus</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="Badge__footer">#Platziconf</div>
       </div>
     );
   }
