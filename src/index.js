6fce5dfe5675520127f8e669759709a95b27e935
renatojobal <renatojobal@gmail.com>
2019-09-22 00:25:17 -0500

    Hasta el inicio de la clase 17

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
 
 const container = document.getElementById("app");
 
 // ReactDOM.render(__que__ (elemento) No recibe componentes, __donde__)
 ReactDOM.render(
-  <BadgeNew
+  <Badges
   />,
   container
 );
