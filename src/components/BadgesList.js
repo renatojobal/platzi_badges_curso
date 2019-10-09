034bd955bac3f7aa72b2754818b9a8d0b0eadeaa
renatojobal <renatojobal@gmail.com>
2019-10-09 16:24:05 -0500

    Creado un react hook

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -26,16 +26,27 @@ class BadgesListItem extends React.Component {
   }
 }
 
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState("");
+
+  const [filteredBadges, setFilteredBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const result = badges.filter((badge) => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+    setFilteredBadges(result);
+  }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
 function BadgesList(props) {
   const badges = props.badges;
 
-  const [query, setQuery] = React.useState("");
-
-  const filteredBadges = badges.filter((badge) => {
-    return `${badge.firstName} ${badge.lastName}`
-      .toLowerCase()
-      .includes(query.toLowerCase());
-  });
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
 
   if (filteredBadges.length === 0) {
     return (
